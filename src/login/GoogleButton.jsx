f5fec28a5c7066cd4236a8e75ae06f61bd986814
marcus <marcus@Marcuss-MacBook-Pro.local>
2021-11-14 22:36:38 +0800

    a

--- src/login/GoogleButton.jsx
+++ src/login/GoogleButton.jsx
@@ -1,17 +1,31 @@
 import { GoogleLogin } from 'react-google-login';
 import { verifyToken } from './requests';
+import { registerUser } from './requests';
 
 export default function Button({token, submit, setToken, setSubmit, setRegister}){
 
     const onSignIn = async (user) => {
         const token = user.getAuthResponse().id_token;
         setToken(token);
+        
+        const profile = user.getBasicProfile();
+        const email = profile.getEmail();
+
         setSubmit(true);
-        console.log(token);
         const verify = await verifyToken(token);
         setSubmit(false);
         if (verify) window.location.reload();
-        else setRegister(true);
+        else {
+            if(email.charAt(0) === 's') setRegister(true);
+            else {
+                setSubmit(true);
+                const register = await registerUser(token);
+                if(register) alert('Registration of NON STUDENT Success');
+                else alert('registration of NON STUDENT unsuccessful');
+                setSubmit(false);
+                window.location.reload();
+            }
+        }
     }
 
     const clientId = '724396208046-174g1j7ib3vhl3foa80j0sd4hvtcv3p9.apps.googleusercontent.com';
