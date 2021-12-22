e9b2f765989dc5a9b1ba6a687426fb98e21628b7
marcus <marcus.imperial01@gmail.com>
2021-12-22 21:28:15 +0800

    a

--- src/login/GoogleButton.jsx
+++ src/login/GoogleButton.jsx
@@ -1,15 +1,15 @@
 import { verifyToken } from './requests';
 import { registerUser } from './requests';
+import { GoogleLogin } from 'react-google-login';
+import GoogleButton from 'react-google-button'
 import { loadGoogleScript } from './google.js';
 import { useEffect } from 'react';
 
 export default function Button({ token, submit, setToken, setSubmit, setRegister}){
+
     const onSignIn = async (user) => {
 
-
-        if(document.readyState !== 'complete') {
-          return;
-        };
+        if(document.readyState !== 'complete') return;
         
         const token = user.getAuthResponse().id_token;
         setToken(token);
@@ -33,64 +33,25 @@ export default function Button({ token, submit, setToken, setSubmit, setRegister
             }
         }
     }
-    const onfail = () => {
-      alert('failed');
-    }
 
     const clientId = '724396208046-174g1j7ib3vhl3foa80j0sd4hvtcv3p9.apps.googleusercontent.com';
 
-    const renderSigninButton = (_gapi) => { // (Ref. 6)
-        _gapi.signin2.render('google-signin', {
-          'scope': 'profile email',
-          'width': 325,
-          'height': 70,
-          'longtitle': true,
-          'theme': 'light',
-          'onsuccess': onSignIn,
-          'onfailure': onfail
-        });
-        alert('rendered')
-      }
-      
-      
-    useEffect(() => {
-
-        
-        // Window.gapi is available at this point
-        window.onGoogleScriptLoad = () => { // (Ref. 1)
-         
-          const _gapi = window.gapi; // (Ref. 2)
-          alert(_gapi);
-          
-          _gapi.load('auth2', () => { // (Ref. 3)
-            (async () => { 
-              const _googleAuth = await _gapi.auth2.init({ // (Ref. 4)
-               client_id: clientId
-              });
-              console.log("TESTETEST");
-              alert(_googleAuth);
-              await _gapi.auth2.getAuthInstance().signOut();
-              renderSigninButton(_gapi); // (Ref. 6)
-            })();
-          });
-        }
-        
-        // Ensure everything is set before loading the script
-        loadGoogleScript(); // (Ref. 9)
-    });
 
 
-    if(!token && !submit)
+
     return (
         <>
-          <header className="container">
-            <section id="sec" className="container">
-              <h1 id="mainlbl">The<br/>SC Website</h1>
-              <div className="googlebutton" id="google-signin"></div>
-            </section>
-          </header>
-            
+
+    <GoogleLogin height={40} width={500}
+    clientId={clientId}
+    render={renderProps => (
+      <GoogleButton height={40} width={500} label='Sign in with Risfamily' type='light' onClick={renderProps.onClick}/>
+    )}
+    onSuccess={onSignIn}
+    cookiePolicy={'single_host_origin'}
+  />
+
         </>
     )
-    else return (<></>)
+
 }
